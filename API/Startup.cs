3598e32d2b25b74f257ced3a7d1c4aff4f573786
Tomer Kedem <tompha1@hotmail.com>
2021-03-23 12:06:30 +0200

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,7 +33,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.addApplicationServices(_config.);
+            services.addApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
             services.AddIdentityService(_config);
