36dbffe58a17d700aa7ed943c2c33f596173b67d
Tomer Kedem <tompha1@hotmail.com>
2021-03-31 22:49:22 +0300

    End of section 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -18,6 +19,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,7 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,    
-    TestErrorsComponent, NotFoundComponent, ServerErrorComponent
+    TestErrorsComponent, NotFoundComponent, ServerErrorComponent, MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +42,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     SharedModule   
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS,  useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS,  useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS,  useClass: JwtInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
