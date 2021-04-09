b6d9354e67dd7d3ff788a371a43514192dff30cc
Tomer Kedem <tompha1@hotmail.com>
2021-04-10 01:07:44 +0300

    End of ssection 15

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DateInputComponent } from './date-input/date-input.component';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,7 @@ import { DateInputComponent } from './date-input/date-input.component';
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,    
-    TestErrorsComponent, NotFoundComponent, ServerErrorComponent, MemberCardComponent, MemberEditComponent, PhotoEditorComponent, TextInputComponent, DateInputComponent
+    TestErrorsComponent, NotFoundComponent, ServerErrorComponent, MemberCardComponent, MemberEditComponent, PhotoEditorComponent, TextInputComponent, DateInputComponent, MemberMessagesComponent
   ],
   imports: [
     BrowserModule,
