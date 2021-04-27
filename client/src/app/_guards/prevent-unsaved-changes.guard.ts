13def845e297a499adc83edabfb0d587b52a6e38
Tomer Kedem <tompha1@hotmail.com>
2021-04-27 08:19:32 +0300

    End of section 18.7 Renew

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,20 @@
+import { MemberEditComponent } from './../members/member-edit/member-edit.component';
+import { Injectable } from '@angular/core';
+import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ConfirmService } from '../_services/confirm.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
+  constructor(private confirmService: ConfirmService){}
+
+  canDeactivate(component: MemberEditComponent): Observable<boolean> | boolean {
+    if(component.editForm.dirty){
+      return this.confirmService.confirm()
+    }
+    return true;
+  }
+  
+}
---
+++ client/src/app/_models/photo.ts
@@ -0,0 +1,9 @@
+
+
+export interface Photo {
+    id: number;
+    url: string;
+    isMain: boolean;
+    isApproved: boolean;
+    username?: string
+}
